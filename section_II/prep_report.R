1af2299d9681d345330d9b91538b2614ff96bd20
ch54321dircd ..dir22 <christian.rutzer@unibas.ch>
2020-09-29 10:10:23 +0200

    network graph with two countries, cited pharma and option to choose domestic citations

--- section_II/prep_report.R
+++ section_II/prep_report.R
@@ -1,4 +1,4 @@
-## Calculate the final data used in the report II in order to reduce real-time calculations within r-markdown / CR 28.9.2020
+## Calculate the final data used in the report II in order to reduce real-time calculations within r-markdown / CR 29.9.2020
 
 library(tidyr)
 library(dplyr)
@@ -32,16 +32,18 @@ citflow_ctry <- subset(citflow_ctry,
 citflow_ctry_temp <- filter(citflow_ctry, tech_field_cited == 16)
 citflow_ctry_temp$tech_name <- "Cited Pharmaceuticals"
 citflow_ctry_temp$tech_field_cited <- 40
-
 citflow_ctry <- rbind(citflow_ctry_temp, citflow_ctry)
 
 # Function to calculate a country's share of backward citations 
 citflow_ctry_func <- function(ctry = "Switzerland"){
-citflow_ctry <- filter(citflow_ctry, ctry_name_cited != ctry & ctry_name_citing==ctry | ctry_name_cited == ctry & tech_name == "Pharmaceuticals")
-citflow_ctry <-subset(citflow_ctry, select = c("tech_field_cited", 
+# citflow_ctry <- filter(citflow_ctry, ctry_name_cited != ctry & ctry_name_citing==ctry | ctry_name_cited == ctry & tech_name == "Pharmaceuticals")
+
+citflow_ctry <- filter(citflow_ctry, ctry_name_citing==ctry)
+citflow_ctry <- subset(citflow_ctry, select = c("tech_field_cited", 
                                                "tech_name", 
                                                "tech_field_citing",
                                                "ctry_name_cited" ,
+                                               "ctry_name_citing", 
                                                "p_year_citing",
                                                "share_inv_cited"))
 
@@ -51,30 +53,32 @@ citflow_ctry %>%
 citflow_ctry$group <- countrycode(sourcevar = citflow_ctry[, "ctry_name_cited"],
                                   origin = "country.name",
                                   destination = "continent") # Creating continent var (group)
+# Keep own country seperate
+citflow_ctry <- mutate(citflow_ctry, group = ifelse(ctry_name_cited == ctry_name_citing, "Domestic", group))
 
 # Agregation summing, without cited pharma patents
 citflow_ctry <- citflow_ctry %>% 
-  group_by(tech_field_citing, tech_field_cited, tech_name, p_year_citing, group) %>% 
+  group_by(tech_field_citing, tech_field_cited, tech_name, p_year_citing, group, ctry_name_citing) %>% 
   summarise(share_inv_cited = sum(share_inv_cited[tech_field_cited!="16"], na.rm = TRUE))
-
-# Create normalized shares per year
-citflow_ctry <- setDT(citflow_ctry)[, total_num := sum(share_inv_cited), .(p_year_citing)] %>%
-  mutate(share_inv_cited = share_inv_cited / total_num)
 
 # Creating a All (continents) group
 citflow_ctry<-citflow_ctry %>% 
-  group_by(tech_field_cited, tech_field_citing, tech_name, p_year_citing) %>% 
-  summarise(group = "All", share_inv_cited = sum(share_inv_cited)) %>%                 
+  group_by(tech_field_cited, tech_field_citing, tech_name, p_year_citing, ctry_name_citing) %>% 
+  summarise(group = "All", share_inv_cited = sum(share_inv_cited[tech_field_cited!="16"], na.rm = TRUE)) %>%
   bind_rows(citflow_ctry, .)
 
+# Create normalized shares per year
+  citflow_ctry <- setDT(citflow_ctry)[, total_num := sum(share_inv_cited[group == "All"], na.rm = T), .(p_year_citing, ctry_name_citing)] %>%
+    mutate(share_inv_cited = share_inv_cited / total_num)
+  
 # Create tech_field_cited by group
 citflow_ctry <- mutate(citflow_ctry, tech_field_cited = ifelse(tech_field_cited != 16, paste0(tech_field_cited, substr(group, 1, 2)), tech_field_cited), ctry_cited = ctry)
 }
 
-citflow_ctry_data <- do.call(rbind.fill, lapply(list("Switzerland", "Germany", "United States", "Italy", "France", "China", "India", "Sweden", "Ireland", "Japan", "Canada", "Israel"), function(x) citflow_ctry_func(x)))
+citflow_ctry_data <- do.call(rbind.fill, lapply(list("Switzerland", "Germany", "United States", "Italy", "France", "China", "India", "Sweden", "Japan"), function(x) citflow_ctry_func(x)))
 
 # Keep only flows from Asia, Americas and Europe; others are irrelevant
-citflow_ctry_data <- filter(citflow_ctry_data, group %in% c("Asia", "Europe", "Americas", "All"))
+citflow_ctry_data <- filter(citflow_ctry_data, group %in% c("Asia", "Europe", "Americas", "All", "Domestic"))
 citflow_ctry_data %>% saveRDS("/scicore/home/weder/rutzer/innoscape/part II descriptive analysis/report/citflow_ctry.rds")
 
 
